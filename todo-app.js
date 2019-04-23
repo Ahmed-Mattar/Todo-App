9ab81d59959ffd03d8b7fa836b1833a2abfaf564
Ahmed Mattar <AceMattar@gmail.com>
2019-04-23 15:37:00 +0200

    feat:render dynamic elements

---
+++ todo-app.js
@@ -0,0 +1,32 @@
+const todos = [{
+    text:'Finish Javascript',
+    completed:false
+}, {
+    text:'Have lunch',
+    completed: true
+}, {
+    text:'Finish Node',
+    completed:false
+}, {
+    text:'Drink Coffe',
+    completed:true
+}, {
+    text: 'Excercise',
+    completed: false
+}];
+
+
+
+const incompleteTodos = todos.filter(function(todo){
+    return !todo.completed;
+})
+
+const summary = document.createElement('h2');
+summary.textContent = `You have ${incompleteTodos.length} todos left`;
+document.querySelector('body').appendChild(summary);
+
+todos.forEach(function(todo){
+    const p = document.createElement('p');
+    p.textContent = todo.text;
+    document.querySelector('body').appendChild(p);
+});
