fe985203af02ea22e0075458c32ab0edfe410077
Ahmed Mattar <AceMattar@gmail.com>
2019-05-13 00:23:41 +0200

    feat:adding event listeners

--- todo-app.js
+++ todo-app.js
@@ -21,6 +21,7 @@ const incompleteTodos = todos.filter(function(todo){
     return !todo.completed;
 })
 
+
 const summary = document.createElement('h2');
 summary.textContent = `You have ${incompleteTodos.length} todos left`;
 document.querySelector('body').appendChild(summary);
@@ -29,4 +30,8 @@ todos.forEach(function(todo){
     const p = document.createElement('p');
     p.textContent = todo.text;
     document.querySelector('body').appendChild(p);
+});
+
+document.querySelector('button').addEventListener('click',function(e) {
+    console.log('i am adding a new todo');
 });
