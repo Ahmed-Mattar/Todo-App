3c3c20594e50779f5283892d56fbad40a3bd54a8
Ahmed Mattar <AceMattar@gmail.com>
2019-05-29 13:52:40 +0200

    feat:using checkbox to show or hide todos

--- todo-app.js
+++ todo-app.js
@@ -1,57 +1,68 @@
 const todos = [{
-    text:'Finish Javascript',
-    completed:false
+    text: 'Order cat food',
+    completed: false
 }, {
-    text:'Have lunch',
+    text: 'Clean kitchen',
     completed: true
 }, {
-    text:'Finish Node',
-    completed:false
+    text: 'Buy food',
+    completed: true
 }, {
-    text:'Drink Coffe',
-    completed:true
-}, {
-    text: 'Excercise',
+    text: 'Do work',
     completed: false
-}];
+}, {
+    text: 'Exercise',
+    completed: true
+}]
 
-
-// Starts
-const filter = {
-    searchText:' '
+const filters = {
+    searchText: '',
+    hideCompleted: false
 }
 
-const renderTodos = function(todos){
-    document.querySelector('#todos').innerHTML = '';
-    todos.forEach(function(todo){
-        const p = document.createElement('p');
-        p.innerText = todo.text;
-        document.querySelector('#todos').appendChild(p);
-    });
+const renderTodos = function (todos, filters) {
+    const filteredTodos = todos.filter(function (todo) {
+        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+        const hideCompletedMatch = !filters.hideCompleted || !todo.completed
+
+        return searchTextMatch && hideCompletedMatch
+    })
+
+    const incompleteTodos = filteredTodos.filter(function (todo) {
+        return !todo.completed
+    })
+
+    document.querySelector('#todos').innerHTML = ''
+
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${incompleteTodos.length} todos left`
+    document.querySelector('#todos').appendChild(summary)
+
+    filteredTodos.forEach(function (todo) {
+        const p = document.createElement('p')
+        p.textContent = todo.text
+        document.querySelector('#todos').appendChild(p)
+    })
 }
 
-renderTodos(todos);
+renderTodos(todos, filters)
 
-const filterTodos = function(todos,filter){
-    const filteredTodos = todos.filter(function(todo){
-        return todo.text.toLowerCase().includes(filter.searchText.toLowerCase());
-    });
-    return filteredTodos;
-}
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
+})
 
+document.querySelector('#new-todo').addEventListener('submit', function (e) {
+    e.preventDefault()
+    todos.push({
+        text: e.target.elements.text.value,
+        completed: false
+    })
+    renderTodos(todos, filters)
+    e.target.elements.text.value = ''
+})
 
-document.querySelector('#search-text').addEventListener('input',function(e){
-    filter.searchText = e.target.value;
-    renderTodos(filterTodos(todos,filter));
-});
-
-document.querySelector('#new-todo').addEventListener('submit',function(e){
-    e.preventDefault();
-    const newTodo = {
-        text:e.target.elements.text.value,
-        completed:false
-    }
-    todos.push(newTodo);
-    renderTodos(todos);
-    e.target.elements.text.value = '';
-});
+document.querySelector('#hide-completed').addEventListener('change', function (e) {
+    filters.hideCompleted = e.target.checked
+    renderTodos(todos, filters)
+})
