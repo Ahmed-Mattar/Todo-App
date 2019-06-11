bc5df18a6811b7be576f25fb147489e52f212560
Ahmed Mattar <AceMattar@gmail.com>
2019-06-11 15:32:44 +0200

    feat:saving data to browser localStorage

--- todo-app.js
+++ todo-app.js
@@ -1,19 +1,11 @@
-const todos = [{
-    text: 'Order cat food',
-    completed: false
-}, {
-    text: 'Clean kitchen',
-    completed: true
-}, {
-    text: 'Buy food',
-    completed: true
-}, {
-    text: 'Do work',
-    completed: false
-}, {
-    text: 'Exercise',
-    completed: true
-}]
+let todos = []
+
+
+const todosJSON = localStorage.getItem('todos');
+
+if(todosJSON !== null){
+    todos = JSON.parse(todosJSON);
+}
 
 const filters = {
     searchText: '',
@@ -58,6 +50,7 @@ document.querySelector('#new-todo').addEventListener('submit', function (e) {
         text: e.target.elements.text.value,
         completed: false
     })
+    localStorage.setItem('todos',JSON.stringify(todos));
     renderTodos(todos, filters)
     e.target.elements.text.value = ''
 })
